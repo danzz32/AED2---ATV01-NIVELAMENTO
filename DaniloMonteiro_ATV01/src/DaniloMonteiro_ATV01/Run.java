d9c2271fa5679233e4b9612f13a62f2e239a6bbc
Danilo Santos Monteiro <103226119+danzz32@users.noreply.github.com>
2023-02-25 10:39:56 -0300

    Add files via upload

--- DaniloMonteiro_ATV01/src/DaniloMonteiro_ATV01/Run.java
+++ DaniloMonteiro_ATV01/src/DaniloMonteiro_ATV01/Run.java
@@ -2,13 +2,11 @@ package DaniloMonteiro_ATV01;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Run {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) {
         String info, linha;
 
         ArvoreBinaria arvore = new ArvoreBinaria();
@@ -23,31 +21,29 @@ public class Run {
 
             linha = buffer.readLine();
             String[] conteudoLinha = linha.split(";");
-
+            //Switch para determinar qual estrutura será instanciada na leitura do arquivo
             switch (conteudoLinha[0]) {
                 case "arvore":
                     linha = buffer.readLine();
                     arvore.inserirArquivo();
                     while (linha != null) {
-                        linha.split(";");
-                        switch (conteudoLinha[0]) {
+                        conteudoLinha = linha.split(";");
+                        switch (conteudoLinha[0]) { // Switch para determinar o método a ser executado
                             case "inserir":
                                 info = conteudoLinha[1];
-                                arvore.inserirObjeto(info);
+                                if (arvore.inserirObjeto(info) == true) {
+                                    System.out.println("Objeto inserido!");
+                                }
                                 break;
                             case "remover":
-                                boolean confirm;
-                                info = conteudoLinha[1];
-                                confirm = arvore.remover(info);
-                                if (confirm == false) {
-                                    System.out.println("Objeto não encontrado");
+                                info = conteudoLinha[0];
+                                if (arvore.remover(info) == false) {
+                                    System.out.println("Não foi possível remover o objeto!");
                                 }
                                 break;
                             case "buscar":
-                                String teste;
-                                info = conteudoLinha[1];
-                                teste = (String) arvore.buscar(info);
-                                if (teste == null) {
+                                info = conteudoLinha[0];
+                                if (arvore.buscar(info) == null) {
                                     System.out.println("Objeto não encontrado!");
                                 }
                                 break;
@@ -55,17 +51,105 @@ public class Run {
                                 arvore.imprimir();
                                 break;
                         }
+                        linha = buffer.readLine();
                     }
                     break;
                 case "lista":
+                    linha = buffer.readLine();
+                    lista.inserirArquivo();
+                    while (linha != null) {
+                        conteudoLinha = linha.split(";");
+                        switch (conteudoLinha[0]) {
+                            case "inserir":
+                                info = conteudoLinha[1];
+                                if (lista.inserirObjeto(info) == true) {
+                                    System.out.println("Objeto inserido!");
+                                }
+                                break;
+                            case "remover":
+                                info = conteudoLinha[0];
+                                if (lista.remover(info) == false) {
+                                    System.out.println("Objeto não encontrado!");
+                                }
+                                break;
+                            case "buscar":
+                                info = conteudoLinha[0];
+                                if (lista.buscar(info) == null) {
+                                    System.out.println("Objeto não encontrado!");
+                                }
+                                break;
+                            case "imprimir":
+                                lista.imprimir();
+                                break;
+                        }
+                        linha = buffer.readLine();
+                    }
                     break;
                 case "pilha":
+                    linha = buffer.readLine();
+                    pilha.inserirArquivo();
+                    while (linha != null) {
+                        conteudoLinha = linha.split(";");
+                        switch (conteudoLinha[0]) {
+                            case "inserir":
+                                info = conteudoLinha[1];
+                                if (pilha.inserirObjeto(info) == true) {
+                                    System.out.println("Objeto inserido!");
+                                }
+                                break;
+                            case "remover":
+                                info = conteudoLinha[0];
+                                if (pilha.remover(info) == false) {
+                                    System.out.println("Objeto não encontrado!");
+                                }
+                                break;
+                            case "buscar":
+                                info = conteudoLinha[0];
+                                if (pilha.buscar(info) == null) {
+                                    System.out.println("Objeto não encontrado!");
+                                }
+                                break;
+                            case "imprimir":
+                                pilha.imprimir();
+                                break;
+                        }
+                        linha = buffer.readLine();
+                    }
                     break;
                 case "fila":
+                    linha = buffer.readLine();
+                    fila.inserirArquivo();
+                    while (linha != null) {
+                        conteudoLinha = linha.split(";");
+                        switch (conteudoLinha[0]) {
+                            case "inserir":
+                                info = conteudoLinha[1];
+                                if (fila.inserirObjeto(info) == true) {
+                                    System.out.println("Objeto inserido!");
+                                }
+                                break;
+                            case "remover":
+                                info = conteudoLinha[0];
+                                if (fila.remover(info) == false) {
+                                    System.out.println("Objeto não encontrado!");
+                                }
+                                break;
+                            case "buscar":
+                                info = conteudoLinha[0];
+                                if (fila.buscar(info) == null) {
+                                    System.out.println("Objeto não encontrado!");
+                                }
+                                break;
+                            case "imprimir":
+                                fila.imprimir();
+                                break;
+                        }
+                        linha = buffer.readLine();
+                    }
                     break;
             }
 
-        } catch (NumberFormatException e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
